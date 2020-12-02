81f84bd9d9434dfc6ab8242f7fdcd03134f16f97
baillaph <baillaph@uqtr.ca>
2020-12-02 18:43:22 -0500

    petits changements mineurs

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import beans.CoursBean;
+import beans.EtudiantBean;
+import beans.InscriptionBean;
 import pm.PersistentManager;
 
 import java.lang.reflect.InvocationTargetException;
@@ -8,8 +10,8 @@ import java.util.List;
 public class Main<T> {
     public static void main(String[] args) throws SQLException, NoSuchMethodException, IllegalAccessException, InvocationTargetException { //Je crée un
         PersistentManager db = new PersistentManager(); //Persisten
-        //List<CoursBean> listcours = db.retrieveSet(CoursBean.class, "SELECT * FROM cours"); //Retourne une liste de cours
-        db.retrieveSet(CoursBean.class, "SELECT * FROM cours"); //Retourne une liste de cours
+        List<CoursBean> listcours = db.retrieveSet(CoursBean.class, "SELECT * FROM cours"); //Retourne une liste de cours
         //Annotation dbIgnore sur les champs qu'on veut pas chercher
+        System.out.println(listcours);
     }
 }
--- src/pm/PersistentManager.java
+++ src/pm/PersistentManager.java
@@ -5,6 +5,7 @@ import beans.EtudiantBean;
 import beans.InscriptionBean;
 import pm.annotations.DbEntity;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.*;
@@ -19,25 +20,55 @@ public class PersistentManager<T> {         //Objectif : effectuer les requêtes
         // 1. Get a connection to database
         this.connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/TP2OO", "postgres", "196432");
     }
-    //public List<T> retrieveSet(Class<T> beanClass, String sql) throws SQLException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-    public void retrieveSet(Class<T> beanClass, String sql) throws SQLException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
-        Class<?> coursClass = CoursBean.class;
-        DbEntity annot = coursClass.getAnnotation(DbEntity.class);
-        System.out.println(annot);
+    public List<T> retrieveSet(Class<T> tClass, String sqlRequest) {
+        List<T> listTObject = null;
+        T tObject;
+
+        try {
+            listTObject = new ArrayList<>();
+            Field[] fields = tClass.getDeclaredFields();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlRequest);
+
+            while (resultSet.next()) {
+                tObject = tClass.newInstance();
+
+                for (Field fieldT : fields) {
+                    fieldT.setAccessible(true);
+                    String nameField = fieldT.getName();
+                    String typeField = fieldT.getType().getTypeName();
+                    switch (typeField) {
+                        case "java.lang.String":
+                            fieldT.set(tObject, resultSet.getString(nameField));
+                            break;
+                        case "int":
+                            fieldT.setInt(tObject, resultSet.getInt(nameField));
+                            break;
+                    }
+                }
+
+                listTObject.add(tObject);
+            }
+        } catch (InstantiationException | SQLException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
 
-            //Créer une classe qui :
-            // - Vérifier annotation présente
-            // - Vérifier les champs
+
+
+
+
+        return listTObject;
+
+        //Créer une classe qui :
+        // - Vérifier annotation présente
+        // - Vérifier les champs
 
     }
-    public void createConnection(){
 
 
-    }
-
-    public void insert(T t) throws SQLException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-
-    }
+//    public void insert(T t) throws SQLException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+//
+//    }
 }
