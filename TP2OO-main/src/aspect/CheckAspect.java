c12aa20349571ea1365e754333a2b06b64041e1b
baillaph <baillaph@uqtr.ca>
2020-12-13 11:34:49 -0500

    log exception en cours de developpement

--- TP2OO-main/src/aspect/CheckAspect.java
+++ TP2OO-main/src/aspect/CheckAspect.java
@@ -1,13 +1,30 @@
 package aspect;
 
 import beans.CoursBean;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
+import java.lang.reflect.Method;
+import java.util.logging.Logger;
+
 public class CheckAspect implements MethodInterceptor {
+
 
     @Override
     public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        try{
+            Object[] objects = methodInvocation.getArguments();
+            String className = this.getClass().getName();
+            //faudra mettre ça générique
+            Method method = this.getClass().getMethod("retrieveSet");
+            Logger log = Logger.getLogger(className);
+            Object result = methodInvocation.proceed();
+        }
+        catch (Exception e){
+            //fiare dequoi pour intercepter le result
+        }
+        
 
 
 
